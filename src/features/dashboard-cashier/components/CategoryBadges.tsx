3e5a1fa52b5803ec0ec836a06ce863db41a99eff
RapzzKY24 <paymeriskiaulia@gmail.com>
2026-02-14 15:09:58 +0700

    refacto : change mapping categories button

--- src/features/dashboard-cashier/components/CategoryBadges.tsx
+++ src/features/dashboard-cashier/components/CategoryBadges.tsx
@@ -11,12 +11,10 @@ type Category = {
 const CategoryBadges = () => {
   const [activeCategory, setActiveCategory] = useState("All Items");
 
-  const { data: categories = [], isLoading } = useQuery<Category[]>({
+  const { data: categories, isLoading } = useQuery<Category[]>({
     queryKey: ["categories"],
     queryFn: dashboardCashier.getCategories,
   });
-
-  const allCategories = ["All Items", ...categories.map((cat) => cat.name)];
 
   return (
     <div className="flex w-full gap-3 overflow-x-auto pb-1 scrollbar-hide">
@@ -30,19 +28,33 @@ const CategoryBadges = () => {
           ))}
         </div>
       ) : (
-        allCategories.map((cat) => (
-          <button
-            key={cat}
-            onClick={() => setActiveCategory(cat)}
-            className={`flex-none rounded-full px-6 py-2.5 text-sm font-semibold transition-all duration-200 ${
-              activeCategory === cat
-                ? "bg-[#3A5A2A] text-white shadow-md shadow-green-900/10 scale-105"
-                : "bg-white text-gray-500 hover:bg-[#E9F5E0] hover:text-[#3A5A2A]"
-            }`}
-          >
-            {cat}
-          </button>
-        ))
+        <>
+          {
+            <button
+              onClick={() => setActiveCategory("All Items")}
+              className={`flex-none rounded-full px-6 py-2.5 text-sm font-semibold transition-all duration-200 ${
+                activeCategory === "All Items"
+                  ? "bg-[#3A5A2A] text-white shadow-md shadow-green-900/10 scale-105"
+                  : "bg-white text-gray-500 hover:bg-[#E9F5E0] hover:text-[#3A5A2A]"
+              }`}
+            >
+              All Items
+            </button>
+          }
+          {categories?.map((cat) => (
+            <button
+              key={cat.id}
+              onClick={() => setActiveCategory(cat.name)}
+              className={`flex-none rounded-full px-6 py-2.5 text-sm font-semibold transition-all duration-200 ${
+                activeCategory === cat.name
+                  ? "bg-[#3A5A2A] text-white shadow-md shadow-green-900/10 scale-105"
+                  : "bg-white text-gray-500 hover:bg-[#E9F5E0] hover:text-[#3A5A2A]"
+              }`}
+            >
+              {cat.name}
+            </button>
+          ))}
+        </>
       )}
     </div>
   );
