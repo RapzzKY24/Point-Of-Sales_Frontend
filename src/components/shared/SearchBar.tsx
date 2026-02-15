a31f78894445728796594970f866b479174ff675
RapzzKY24 <paymeriskiaulia@gmail.com>
2026-02-15 18:23:00 +0700

    feat: add props in seach bar

--- src/components/shared/SearchBar.tsx
+++ src/components/shared/SearchBar.tsx
@@ -2,17 +2,30 @@ import { BarcodeIcon, SearchIcon } from "lucide-react";
 import { Button } from "../ui/Button";
 import { Input } from "../ui/Input";
 
-const SearchBar = () => {
+type SearchBarProps = {
+  searchValue: string;
+  onSearchChange: (value: string) => void;
+  onScanBarcode?: () => void;
+};
+
+const SearchBar = ({
+  searchValue,
+  onSearchChange,
+  onScanBarcode,
+}: SearchBarProps) => {
   return (
     <div className="flex gap-4">
       <div className="relative flex-1 group">
         <SearchIcon className="absolute left-4 top-1/2 size-5 -translate-y-1/2 text-gray-400 group-focus-within:text-[#5F8D4E] transition-colors" />
         <Input
+          value={searchValue}
+          onChange={(e) => onSearchChange(e.target.value)}
           className="h-14 w-full rounded-2xl border-0 bg-white pl-12 shadow-sm text-base ring-1 ring-gray-100 placeholder:text-gray-400 focus-visible:ring-2 focus-visible:ring-[#5F8D4E]"
           placeholder="Search product name or SKU..."
         />
       </div>
       <Button
+        onClick={onScanBarcode}
         variant="outline"
         className="h-14 gap-3 rounded-2xl border-gray-200 bg-white px-8 text-gray-700 shadow-sm hover:bg-gray-50 hover:text-[#5F8D4E] hover:border-[#5F8D4E]"
       >
